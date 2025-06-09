d4e46a02cd5e2280389811ad5ed071cf31a264f1
Suraj Chavan <surajchavan99886@gmail.com>
2025-06-09 21:50:33 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors(
-    origin.process.env.CORS_ORIGIN
-))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
 
 app.use(express.json({
     limit:"16kb"
@@ -18,4 +20,8 @@ app.use(express.static("public"))
 
 app.use(express.urlencoded({extended:true}))
 
+// routes 
+import userRouter from "./routes/user.routes.js"
+// routes declaration 
+app.use("/user",userRouter)
 export {app}
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
+        // console.log(MONGODB_URI)
         const connect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\nMongoDB connected !! DB HOST: ${connect.connection.host}`);
     } catch (error) {
