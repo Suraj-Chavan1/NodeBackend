1c7e763d344992c69f799e0ff3710475671500b5
Suraj Chavan <surajchavan99886@gmail.com>
2025-06-10 17:26:31 +0530

    jwt added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,20 +1,28 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controller.js";
+import { registerUser ,loginUser } from "../controllers/user.controller.js";
+import {logoutUser} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name : "avatar" , 
-            maxCount : 1
-        } ,
+            name: "avatar",
+            maxCount: 1
+        }, 
         {
-            name : "coverImage" ,
-            maxCount : 1
+            name: "coverImage",
+            maxCount: 1
         }
-    ]) ,
-    registerUser)
+    ]),
+    registerUser
+    )
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT , logoutUser)
 
 
 export default router 
